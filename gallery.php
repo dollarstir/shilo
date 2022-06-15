117537871e3bf4bd2834292d8cb4d2dd6bb60562
dollarstir <kpin463@gmail.com>
2022-06-15 16:04:06 +0000

    dsa

--- gallery.php
+++ gallery.php
@@ -129,7 +129,7 @@ div.desc {
       <div>
 
         <div class="w3-card-4 w3-light-gray" style="width:100%!important;height:80%;">
-        <img loading="lazy" style="margin-top: 5px;width:100%!important;height:100%!important;border-radius:0px!important;" src="assets/shil/1.JPG" alt="Avatar">
+        <img loading="lazy" style="margin-top: 5px;width:100%!important;height:100%!important;border-radius:0px!important;" src="assets/shil/1.jpg" alt="Avatar">
           <div class="w3-container w3-center">
           
           </div>
@@ -141,7 +141,7 @@ div.desc {
       <div>
 
         <div class="w3-card-4 w3-light-gray" style="width:100%!important;height:80%;">
-        <img loading="lazy" style="margin-top: 5px;width:100%!important;height:100%!important;border-radius:0px!important;" src="assets/shil/13.JPG" alt="Avatar">
+        <img loading="lazy" style="margin-top: 5px;width:100%!important;height:100%!important;border-radius:0px!important;" src="assets/shil/13.jpg" alt="Avatar">
           <div class="w3-container w3-center">
           
           </div>
@@ -153,7 +153,7 @@ div.desc {
       <div>
 
         <div class="w3-card-4 w3-light-gray" style="width:100%!important;height:80%;">
-        <img loading="lazy" style="margin-top: 5px;width:100%!important;height:100%!important;border-radius:0px!important;" src="assets/shil/12.JPG" alt="Avatar">
+        <img loading="lazy" style="margin-top: 5px;width:100%!important;height:100%!important;border-radius:0px!important;" src="assets/shil/12.jpg" alt="Avatar">
           <div class="w3-container w3-center">
           
           </div>
